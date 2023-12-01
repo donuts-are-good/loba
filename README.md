73ad34033837566de22441c683a42e5ca8eff7fe
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-12-01 02:08:11 -0600

    adding readme

--- README.md
+++ README.md
@@ -1,7 +1,58 @@
+![logo](https://github.com/donuts-are-good/imghost/assets/96031819/46c17259-549a-4395-ab10-a4c5814f974e)
+![donuts-are-good's followers](https://img.shields.io/github/followers/donuts-are-good?&color=555&style=for-the-badge&label=followers) ![donuts-are-good's stars](https://img.shields.io/github/stars/donuts-are-good?affiliations=OWNER%2CCOLLABORATOR&color=555&style=for-the-badge) ![donuts-are-good's visitors](https://komarev.com/ghpvc/?username=donuts-are-good&color=555555&style=for-the-badge&label=visitors)
+
+
 # loba
+loba is a load balancer written in go. it's experimental so maybe dont use it at work yet.
 
-## usage
+# what
+loba uses a round-robin algorithm to distribute incoming http requests across multiple servers. it also logs each request and response, storing the data in a sqlite database. you can then generate reports on the total number of requests per domain and the most recent request time.
 
-## license
+# domains config
+all you need to do is create a domains.json file in the config directory. here's an example:
+
+```json
+[
+  {
+    "domain": "example1.com",
+    "servers": [
+      "http://server1.example.com",
+      "http://server2.example.com"
+    ]
+  },
+  {
+    "domain": "example2.com",
+    "servers": [
+      "http://server3.example.com",
+      "http://server4.example.com"
+    ]
+  }
+]
+```
+
+each object in the array represents a domain and the servers that will handle requests for that domain.
+
+# health check
+loba also includes a health check endpoint at /health. it's a simple way to verify that the load balancer is up and running.
+
+# reporting
+want to see how your servers are doing? just hit the /report endpoint. you'll get a json response with the total number of requests and the most recent request time for each domain.
+
+# neosay config
+here's an example of a neosay config:
+
+```json
+{
+  "HomeserverURL": "https://matrix.org",
+  "UserID": "@myusername:matrix.org",
+  "AccessToken": "sy_uegfiubwefiouh98h38u4hgr34uy",
+  "Rooms": {
+    "error": "!NvGwefy34y3lIy:matrix.org",
+    "general": "!yzLf3445hheeQR:matrix.org",
+    "request": "!Cydgojiihergsgk:matrix.org",
+    "launch": "!WwehbewuybgweaPC:matrix.org"
+  }
+}
+```
 
 MIT License 2023 donuts-are-good, for more info see license.md
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"math/rand"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -16,7 +17,19 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var BuildNumber = "1"
+var (
+	nodeHealthCheckCycleTime = 60 * time.Second
+	timeout                  = time.Second * 2
+	BuildNumber              = "4"
+	db                       *sqlx.DB
+	servers                  map[string][]Server
+	current                  int
+	configMutex              = &sync.Mutex{}
+	initLogsTable            = `CREATE TABLE IF NOT EXISTS logs ( request_id TEXT, request_ip TEXT, user_agent TEXT, server_addr TEXT, domain TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, http_response INTEGER);`
+	insertLogEntry           = `INSERT INTO logs (request_id, request_ip, user_agent, server_addr, domain, http_response) VALUES (?, ?, ?, ?, ?, ?)`
+	enumerateDomainStats     = `SELECT domain, COUNT(*) AS total_requests, MAX(timestamp) AS most_recent_time
+			FROM logs GROUP BY domain`
+)
 
 type Server struct {
 	Addr   string
@@ -35,72 +48,62 @@ type DomainConfig struct {
 	Servers []string `json:"servers"`
 }
 
-var db *sqlx.DB
-var servers map[string][]Server
-var current int
-var configMutex = &sync.Mutex{}
-
 func main() {
 	var err error
+	nodeHealthCheckCycle()
+	initSqlite(err)
+	loadConfig()
+	routeHandlers()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func routeHandlers() {
+	http.HandleFunc("/", proxyHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/report", reportHandler)
+	http.HandleFunc("/reload", reloadHandler)
+}
+
+func initSqlite(err error) {
 	db, err = sqlx.Connect("sqlite3", "./loadbalancer.db")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS logs (
-		request_id TEXT,
-		request_ip TEXT,
-		user_agent TEXT,
-		server_addr TEXT,
-		domain TEXT,
-		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-		http_response INTEGER
-	);`)
+	_, err = db.Exec(initLogsTable)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	loadConfig()
-
-	go func() {
-		for range time.Tick(60 * time.Second) {
-			loadConfig()
-		}
-	}()
-
-	http.HandleFunc("/", proxyHandler)
-	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/report", reportHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func loadConfig() {
-	configMutex.Lock()
-	defer configMutex.Unlock()
-
+func loadConfig() map[string][]Server {
 	data, err := os.ReadFile("./config/domains.json")
 	if err != nil {
 		log.Println("Error reading domains.json:", err)
-		return
+		return nil
 	}
 
 	var config []DomainConfig
 	err = json.Unmarshal(data, &config)
 	if err != nil {
 		log.Println("Error parsing domains.json:", err)
-		return
+		return nil
 	}
 
-	servers = make(map[string][]Server)
+	newServers := make(map[string][]Server)
 	for _, domainConfig := range config {
 		for _, serverAddr := range domainConfig.Servers {
-			servers[domainConfig.Domain] = append(servers[domainConfig.Domain], Server{
-				Addr:   serverAddr,
-				Domain: domainConfig.Domain,
-				Active: true,
-			})
+			if checkServerHealth(serverAddr) {
+				newServers[domainConfig.Domain] = append(newServers[domainConfig.Domain], Server{
+					Addr:   serverAddr,
+					Domain: domainConfig.Domain,
+					Active: true,
+				})
+			}
 		}
 	}
+
+	return newServers
 }
 
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
@@ -113,8 +116,19 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	server := domainServers[current]
-	current = (current + 1) % len(domainServers)
+	var server Server
+	for i := 0; i < len(domainServers); i++ {
+		server = domainServers[current]
+		current = (current + 1) % len(domainServers)
+		if server.Active {
+			break
+		}
+	}
+
+	if !server.Active {
+		http.Error(w, "No active servers available for domain", http.StatusInternalServerError)
+		return
+	}
 
 	target, err := url.Parse(server.Addr)
 	if err != nil {
@@ -127,6 +141,14 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	proxy.ServeHTTP(w, r)
 }
 
+func nodeHealthCheckCycle() {
+	go func() {
+		for range time.Tick(nodeHealthCheckCycleTime) {
+			checkAllServerHealth()
+		}
+	}()
+}
+
 func logResponse(r *http.Response) error {
 	requestID := generateHash(32)
 	requestIP := strings.Split(r.Request.RemoteAddr, ":")[0]
@@ -134,8 +156,7 @@ func logResponse(r *http.Response) error {
 	serverAddr := r.Request.URL.Host
 	domain := r.Request.Host
 
-	_, err := db.Exec(`INSERT INTO logs (request_id, request_ip, user_agent, server_addr, domain, http_response)
-		VALUES (?, ?, ?, ?, ?, ?)`,
+	_, err := db.Exec(insertLogEntry,
 		requestID, requestIP, userAgent, serverAddr, domain, r.StatusCode)
 	if err != nil {
 		return err
@@ -146,8 +167,7 @@ func logResponse(r *http.Response) error {
 }
 
 func reportHandler(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Queryx(`SELECT domain, COUNT(*) AS total_requests, MAX(timestamp) AS most_recent_time
-		FROM logs GROUP BY domain`)
+	rows, err := db.Queryx(enumerateDomainStats)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -187,4 +207,44 @@ func generateHash(length int) string {
 		result[i] = chars[rand.Intn(len(chars))]
 	}
 	return string(result)
+}
+
+func checkServerHealth(serverAddr string) bool {
+
+	conn, err := net.DialTimeout("tcp", serverAddr+"/v0/health", timeout)
+	if err != nil {
+		log.Println("Server health check failed:", err)
+		return false
+	}
+	conn.Close()
+	return true
+}
+
+func checkAllServerHealth() {
+	configMutex.Lock()
+	defer configMutex.Unlock()
+
+	for domain, domainServers := range servers {
+		for i, server := range domainServers {
+			if !checkServerHealth(server.Addr) {
+				servers[domain] = append(domainServers[:i], domainServers[i+1:]...)
+				log.Println("Server removed:", server.Addr)
+			}
+		}
+	}
+}
+
+func reloadHandler(w http.ResponseWriter, r *http.Request) {
+	newServers := loadConfig()
+	if newServers == nil {
+		http.Error(w, "Invalid configuration", http.StatusInternalServerError)
+		return
+	}
+
+	configMutex.Lock()
+	servers = newServers
+	configMutex.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("Configuration reloaded"))
 }
